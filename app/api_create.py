a85e0510b23ed023b0af1344d04545eba60a4c13
Kushal <am.kushal02@gmail.com>
2024-01-07 23:55:53 +0530

    Add files via upload

---
+++ app/api_create.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+ATLAS_URI = "mongodb+srv://naveen:Z7RHvWPN4orjL74y@edtech.qdjcop3.mongodb.net/?retryWrites=true&w=majority"
+
+
+#==================================================================
+#==================================================================
+
