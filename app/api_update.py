de603f97a6b432e695804fbcb39687bf89b33a1a
Kushal <am.kushal02@gmail.com>
2024-01-10 00:00:04 +0530

    Update api_update.py

--- app/api_update.py
+++ app/api_update.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-ATLAS_URI = "mongodb+srv://naveen:Z7RHvWPN4orjL74y@edtech.qdjcop3.mongodb.net/?retryWrites=true&w=majority"
+ATLAS_URI = "ATLAS_SECRET"
 
 
 #==================================================================
