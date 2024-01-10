79dfe4846b473a6fbd278978c02326fedb0cef2f
Kushal <am.kushal02@gmail.com>
2024-01-10 23:36:35 +0530

    Add files via upload

--- site_admin/api_create_admin.py
+++ site_admin/api_create_admin.py
@@ -7,7 +7,7 @@ import re
 from bs4 import BeautifulSoup
 
 
-ATLAS_URI = "ATLAS_SECRET"
+ATLAS_URI = "mongodb+srv://naveen:Z7RHvWPN4orjL74y@edtech.qdjcop3.mongodb.net/?retryWrites=true&w=majority"
 
 
 def linked_name(title):
